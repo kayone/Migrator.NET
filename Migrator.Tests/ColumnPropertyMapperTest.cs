940064114b2f7d124e1e8fcf832510f4c758adc2
kay.one <kay.one@gmail.com>
2012-03-14 19:32:29 -0700

    Cleaned up solution folder

---
+++ Migrator.Tests/ColumnPropertyMapperTest.cs
@@ -0,0 +1,73 @@
+using System.Data;
+using Migrator.Framework;
+using Migrator.Providers;
+
+using Migrator.Providers.SQLite;
+using Migrator.Providers.SqlServer;
+using NUnit.Framework;
+
+namespace Migrator.Tests
+{
+	[TestFixture]
+	public class ColumnPropertyMapperTest
+	{
+		[Test]
+		public void SqlServerCreatesNotNullSql()
+		{
+			var mapper = new ColumnPropertiesMapper(new SqlServerDialect(), "varchar(30)");
+			mapper.MapColumnProperties(new Column("foo", DbType.String, ColumnProperty.NotNull));
+			Assert.AreEqual("[foo] varchar(30) NOT NULL", mapper.ColumnSql);
+		}
+
+		[Test]
+		public void SqlServerCreatesSqWithBooleanDefault()
+		{
+			var mapper = new ColumnPropertiesMapper(new SqlServerDialect(), "bit");
+			mapper.MapColumnProperties(new Column("foo", DbType.Boolean, 0, false));
+			Assert.AreEqual("[foo] bit DEFAULT 0", mapper.ColumnSql);
+
+			mapper.MapColumnProperties(new Column("bar", DbType.Boolean, 0, true));
+			Assert.AreEqual("[bar] bit DEFAULT 1", mapper.ColumnSql);
+		}
+
+		[Test]
+		public void SqlServerCreatesSqWithDefault()
+		{
+			var mapper = new ColumnPropertiesMapper(new SqlServerDialect(), "varchar(30)");
+			mapper.MapColumnProperties(new Column("foo", DbType.String, 0, "'NEW'"));
+			Assert.AreEqual("[foo] varchar(30) DEFAULT 'NEW'", mapper.ColumnSql);
+		}
+
+		[Test]
+		public void SqlServerCreatesSqWithNullDefault()
+		{
+			var mapper = new ColumnPropertiesMapper(new SqlServerDialect(), "varchar(30)");
+			mapper.MapColumnProperties(new Column("foo", DbType.String, 0, "NULL"));
+			Assert.AreEqual("[foo] varchar(30) DEFAULT NULL", mapper.ColumnSql);
+		}
+
+		[Test]
+		public void SqlServerCreatesSql()
+		{
+			var mapper = new ColumnPropertiesMapper(new SqlServerDialect(), "varchar(30)");
+			mapper.MapColumnProperties(new Column("foo", DbType.String, 0));
+			Assert.AreEqual("[foo] varchar(30)", mapper.ColumnSql);
+		}
+
+		[Test]
+		public void SqlServerIndexSqlIsNoNullWhenIndexed()
+		{
+			var mapper = new ColumnPropertiesMapper(new SqlServerDialect(), "char(1)");
+			mapper.MapColumnProperties(new Column("foo", DbType.StringFixedLength, 1, ColumnProperty.Indexed));
+			Assert.IsNull(mapper.IndexSql);
+		}
+
+        [Test]
+        public void SQLiteIndexSqlWithEmptyStringDefault()
+        {
+            var mapper = new ColumnPropertiesMapper(new SQLiteDialect(), "varchar(30)");
+            mapper.MapColumnProperties(new Column("foo", DbType.String, 1, ColumnProperty.NotNull, string.Empty));
+            Assert.AreEqual("foo varchar(30) NOT NULL DEFAULT ''", mapper.ColumnSql);
+        }
+	}
+}
---
+++ Migrator/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Kayone.Migrator")]
+[assembly: AssemblyDescription("Database migration for .NET")]
+[assembly: AssemblyProduct("Kayone.Migrator")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("0DD57244-8C7F-4F08-96C5-56B12AE23639")]
+[assembly: AssemblyVersion("1.0.0.*")]
